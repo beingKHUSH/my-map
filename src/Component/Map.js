712b8d23796684eb6321b1fc0aa6c7322704761f
Khushboo Jain <30721450+beingKHUSH@users.noreply.github.com>
2018-11-25 20:15:06 +0530

    initial commit

---
+++ src/Component/Map.js
@@ -0,0 +1,24 @@
+import React, { Component } from 'react';
+import { withGoogleMap, GoogleMap } from 'react-google-maps';
+
+class Map extends Component {
+   	render() {
+	   	const GoogleMapExample = withGoogleMap(props => (
+			<GoogleMap
+			defaultCenter = { { lat: 29.0588, lng: 76.0856 } }
+			defaultZoom = { 12 }
+			>
+			</GoogleMap>
+		));
+		return(
+			<div>
+				<GoogleMapExample
+				  containerElement={ <div style={{ height: `100vh`, width: '100vw' }} /> }
+				  mapElement={ <div style={{ height: `100%`, width: `100%`  }} /> }
+				/>
+			</div>
+		);
+	}
+};
+
+export default Map;
