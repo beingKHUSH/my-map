89438b921d6935e70b038c9fb2be472d60e13794
Khushboo Jain <30721450+beingKHUSH@users.noreply.github.com>
2018-12-02 10:10:40 +0530

    map and marker initial code

--- src/Component/Map.js
+++ src/Component/Map.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { withGoogleMap, GoogleMap } from 'react-google-maps';
+import { withGoogleMap, GoogleMap, Marker } from 'react-google-maps';
 
 class Map extends Component {
    	render() {
@@ -8,6 +8,7 @@ class Map extends Component {
 			defaultCenter = { { lat: 29.0588, lng: 76.0856 } }
 			defaultZoom = { 12 }
 			>
+				<Marker position={{lat: 29.0588, lng: 76.0856}}></Marker>
 			</GoogleMap>
 		));
 		return(
